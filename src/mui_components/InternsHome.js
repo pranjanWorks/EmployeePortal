c4dd362e41591abea0ec89659a9600e3c0599140
Pratyush Ranjan <pranjan.work@gmail.com>
2024-01-19 11:59:50 +0530

    Replaced add button with fab

--- src/mui_components/InternsHome.js
+++ src/mui_components/InternsHome.js
@@ -1,25 +1,39 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Fab, Typography } from "@mui/material";
 import React from "react";
 import ListView from "./ListView";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import AddIcon from '@mui/icons-material/Add';
 
 const InternsHome = ({ interns, deleteHandler }) => {
+    const navigate = useNavigate();
+    
     return (
-        <Box
-            height="100vh"
+        <Box 
+            height="100vh" 
             display="flex"
             justifyContent="center"
-            alignItems="center"
-            flexDirection="column"
         >
-            <ListView items={interns} deleteHandler={deleteHandler}></ListView>
-            <Button 
-                variant="outlined"
-                component={Link}
-                to="/add"
+            <Box
+                width="fit-content"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="column"
             >
-                Add New Intern
-            </Button>
+                <ListView items={interns} deleteHandler={deleteHandler}></ListView>
+            </Box>
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="flex-end"
+                width="fit-content"
+                sx={{padding: 1, marginBottom: "7vh"}}
+            >
+                <Fab color="primary" onClick={() => navigate("/add")}>
+                    <AddIcon />
+                </Fab>
+            </Box>
         </Box>
     );
 };
