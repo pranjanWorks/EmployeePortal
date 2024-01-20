b70278e22c1ce3c90fae7cadb1813eedb69a7e15
Pratyush Ranjan <pranjan.work@gmail.com>
2024-01-21 00:58:26 +0530

    mock function simulates useNavigate

--- src/__tests__/AddInternSnapshot.test.js
+++ src/__tests__/AddInternSnapshot.test.js
@@ -1,6 +1,12 @@
 import renderer from 'react-test-renderer';
 import AddIntern from '../mui_components/AddIntern';
 
+const mockUseNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+   ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+}));
+
 it('AddIntern', () => {
     const tree = renderer
         .create(<AddIntern addHandler={() => {}} />)
