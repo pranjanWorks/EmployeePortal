1f9ea553f5ff44b33eaf03b631b83927214a6205
Pratyush Ranjan <pranjan.work@gmail.com>
2024-01-21 23:15:26 +0530

    Added test-ids to Text Fields

--- src/mui_components/EditIntern.js
+++ src/mui_components/EditIntern.js
@@ -44,11 +44,11 @@ const EditIntern = ({ editHandler }) => {
             noValidate
             autoComplete="off"
         >
-            <TextField required id="name" label="Name" value={intern.name} onChange={handleChange}></TextField>
-            <TextField required id="email" label="Email" value={intern.email} onChange={handleChange}></TextField>
-            <TextField required id="department" label="Department" value={intern.department} onChange={handleChange}></TextField>
-            <TextField required id="primaryNo" label="Primary Number" value={intern.primaryNo} onChange={handleChange}></TextField>
-            <Button type="submit" variant="contained">Update</Button>
+            <TextField required id="name" label="Name" value={intern.name} onChange={handleChange} inputProps={{'data-testid': 'name-input'}}></TextField>
+            <TextField required id="email" label="Email" value={intern.email} onChange={handleChange} inputProps={{'data-testid': 'email-input'}}></TextField>
+            <TextField required id="department" label="Department" value={intern.department} onChange={handleChange} inputProps={{'data-testid': 'department-input'}}></TextField>
+            <TextField required id="primaryNo" label="Primary Number" value={intern.primaryNo} onChange={handleChange} inputProps={{'data-testid': 'primaryNo-input'}}></TextField>
+            <Button type="submit" data-testid="update" variant="contained">Update</Button>
 
             {!isFormValid && (
                 <Alert severity="error" sx={{marginTop: 1}}>
