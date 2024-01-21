66b5e18809b0d48100fe752976462e46b98ce7af
Pratyush Ranjan <pranjan.work@gmail.com>
2024-01-21 12:32:48 +0530

    Test added - addHandler and back navigation from AddIntern if successful form submission

--- src/__tests__/RouterTests.test.js
+++ src/__tests__/RouterTests.test.js
@@ -3,6 +3,7 @@
  */
 
 import InternsHome from '../mui_components/InternsHome';
+import AddIntern from '../mui_components/AddIntern';
 import { fireEvent, render } from '@testing-library/react';
 import interns from '../sample_data/interns';
 
@@ -20,5 +21,32 @@ describe('Routing Tests', () => {
         const fabButton = getByTestId("add-button");
         fireEvent.click(fabButton);
         expect(mockUseNavigate).toHaveBeenCalledWith("/add");
+    });
+
+    it('Route back from AddInternScreen upon successful form submission', () => {
+        const mockAddHandler = jest.fn();
+
+        const intern = {
+            name: 'Gautum Rizwani',
+            email: 'gautum.rizwani@ringcentral.com',
+            department: 'Development',
+            primaryNo: '068510 98361'
+        };
+        
+        const { getByTestId } = render(<AddIntern addHandler={mockAddHandler} />);
+        const nameTextField = getByTestId('name-input');
+        const emailTextField = getByTestId('email-input');
+        const departmentTextField = getByTestId('department-input');
+        const primaryNoTextField = getByTestId('primaryNo-input');
+        const submitButton = getByTestId("submit");
+
+        fireEvent.change(nameTextField, {target: {value: intern.name}});
+        fireEvent.change(emailTextField, {target: {value: intern.email}});
+        fireEvent.change(departmentTextField, {target: {value: intern.department}});
+        fireEvent.change(primaryNoTextField, {target: {value: intern.primaryNo}});
+
+        fireEvent.click(submitButton);
+        expect(mockAddHandler).toHaveBeenCalledWith(intern);
+        expect(mockUseNavigate).toHaveBeenCalledWith(-1);
     });
 });
